13f81de18f2b7a3312b4be9b14cdfb4bbd9d39f3
Ser Lev Arris <arris@zsolttech.com>
2014-09-02 23:50:08 +0200

    shininess calculation factor fixed

--- code/ObjFileImporter.cpp
+++ code/ObjFileImporter.cpp
@@ -543,8 +543,7 @@ void ObjFileImporter::createMaterials(const ObjFile::Model* pModel, aiScene* pSc
 	
 		mat->AddProperty<int>( &sm, 1, AI_MATKEY_SHADING_MODEL);
 
-		// multiplying the specular exponent with 2 seems to yield better results
-		pCurrentMaterial->shineness *= 4.f;
+		pCurrentMaterial->shineness *= .128f;
 
 		// Adding material colors
 		mat->AddProperty( &pCurrentMaterial->ambient, 1, AI_MATKEY_COLOR_AMBIENT );
