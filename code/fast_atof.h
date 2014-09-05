1200ba29087c6e52425b639069a1cc7bbafdfe73
U-Satellite-C670\zsolt <zsolt@Satellite-C670.zsolttech.com>
2014-09-05 10:06:17 +0200

    sync from trunk

--- code/fast_atof.h
+++ code/fast_atof.h
@@ -42,12 +42,12 @@ const double fast_atof_table[16] =	{  // we write [16] here instead of [] to wor
 	0.000000000000001
 };
 
+#include <errno.h>
 inline unsigned int hexstrtoul10(const char* in, const char** out = 0) {
 	/* http://stackoverflow.com/questions/4132318/how-to-convert-hex-string-to-unsigned-64bit-uint64-t-integer-in-a-fast-and-saf answer 3. */
 	//char *in, *end;
 	unsigned long long result;
-//      #include <errno.h>
-	int errno = 0;
+	errno = 0;
 	// if (!isxdigit(in[0]) || (in[1] && !isxdigit(in[1])))
 	// if (in[0]=='0' && in[1])
 	//result = strtoull(in, &end, 16);
