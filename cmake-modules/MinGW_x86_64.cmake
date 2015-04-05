7a52e8f513da927c087ba9c5ce192bdb66515a3c
Ser Lev Arris <arris@zsolttech.com>
2015-04-05 13:46:37 +0200

    use full patch to gcc, (check that this sets MINGW-Variables correct.)

--- cmake-modules/MinGW_x86_64.cmake
+++ cmake-modules/MinGW_x86_64.cmake
@@ -1,19 +1,24 @@
-# looks like cmake is a bit braindead, selects c and c++
-# but not ld and ar for crosscompile, so we force it
-INCLUDE(CMakeForceCompiler)
-
+## looks like cmake is a bit braindead, selects c and c++
+## but not ld and ar for crosscompile, so we force it
+#INCLUDE(CMakeForceCompiler)
+#
 # this one sets internal to crosscompile (in theory)
 SET(CMAKE_SYSTEM_NAME Windows)
 
-# extreme way of settings...
-# which C and C++ compiler to use
-SET(_CMAKE_TOOLCHAIN_PREFIX "x86_64-w64-mingw32-")
-SET(CMAKE_RC_COMPILER "${_CMAKE_TOOLCHAIN_PREFIX}windres")
+# the minimalistic setting
+SET(CMAKE_C_COMPILER "/usr/bin/x86_64-w64-mingw32-gcc")
+SET(CMAKE_RC_COMPILER "/usr/bin/x86_64-w64-mingw32-windres")
 
-# specify the force cross compiler else compiler-test fails.
-# strange thing that compiler ID is ignored???
-CMAKE_FORCE_C_COMPILER(${_CMAKE_TOOLCHAIN_PREFIX}gcc MinGW)
-CMAKE_FORCE_CXX_COMPILER(${_CMAKE_TOOLCHAIN_PREFIX}g++ MinGW)
+
+## extreme way of settings...
+## which C and C++ compiler to use
+#SET(_CMAKE_TOOLCHAIN_PREFIX "x86_64-w64-mingw32-")
+#SET(CMAKE_RC_COMPILER "${_CMAKE_TOOLCHAIN_PREFIX}windres")
+
+## specify the force cross compiler else compiler-test fails.
+## strange thing that compiler ID is ignored???
+#CMAKE_FORCE_C_COMPILER(${_CMAKE_TOOLCHAIN_PREFIX}gcc MinGW)
+#CMAKE_FORCE_CXX_COMPILER(${_CMAKE_TOOLCHAIN_PREFIX}g++ MinGW)
 
 # where is the target (so called staging) environment
 SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
