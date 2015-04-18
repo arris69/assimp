457831c629227b7e5a754b7ed75a554a6c96a4bf
Ser Lev Arris <arris@zsolttech.com>
2015-04-18 12:11:09 +0200

    includes don't use '..' folder definition
    1. step with:
    sed -i -e "s/\(\#include \"\)\.\..*\(assimp.*\)/\1\2/g" ../../code/*.*

--- code/PlyExporter.cpp
+++ code/PlyExporter.cpp
@@ -46,11 +46,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <boost/scoped_ptr.hpp>
 #include <cmath>
 #include "Exceptional.h"
-#include "../include/assimp/scene.h"
-#include "../include/assimp/version.h"
-#include "../include/assimp/IOSystem.hpp"
-#include "../include/assimp/Exporter.hpp"
-#include "qnan.h"
+#include "assimp/scene.h"
+#include "assimp/version.h"
+#include "assimp/IOSystem.hpp"
+#include "assimp/Exporter.hpp"
 
 
 using namespace Assimp;
